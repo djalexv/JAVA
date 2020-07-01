61df33c906c45c0900d1e81604df9d4ad340671d
artemenkoav <alex.4soft@mail.ru>
2020-07-01 16:18:41 +0700

    00 test

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -2,6 +2,7 @@ public class PhoneBook {
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
+        //rjvtyn
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
