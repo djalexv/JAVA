47469e3308aa52688f182399fc1b628696afd29a
artemenkoav <alex.4soft@mail.ru>
2020-07-01 20:10:59 +0700

    add methods -  stringArrayToString,  checkPhoneNumberIsCorrect -> checkPhoneNumber

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class PhoneBook {
 
     public static void main(String[] args) {
-//        String[] currentNameArray = getName();
-//        System.out.println(Arrays.toString(currentNameArray));
+        String[] currentNameArray = getName();
+        System.out.println(Arrays.toString(currentNameArray));
         String currentNumber = getNumber();
-        System.out.println("000 "+currentNumber);
+        System.out.println(stringArrayToString(currentNameArray)+ " " + currentNumber);
 
 
     }
@@ -20,36 +20,31 @@ public class PhoneBook {
             System.out.println("Введите номер телефона - 11 цифр. Первая 7 или 8");
             phoneNumber = cleanPhoneNumber(inData.nextLine()); //Считывает строку из System.in
 //            phoneNumber = cleanPhoneNumber(phoneNumber);
-            isCorrectNumber = checkPhoneNumberIsCorrect(phoneNumber); //checkPhoneNumberIsCorrect(phoneNumber);
+            isCorrectNumber = checkPhoneNumber(phoneNumber); //checkPhoneNumberIsCorrect(phoneNumber);
             if (!isCorrectNumber) {
                 System.out.println("Введите корректный номер!");
-            } else {
-                inData.close();
-//                phoneNumber = formatPhoneNumber(phoneNumber);
-//                System.out.println();
-                System.out.println("11 " + phoneNumber+ " 22 "+formatNumber(phoneNumber));
-
             }
         }
+        inData.close();
+        phoneNumber = formatPhoneNumber(phoneNumber);
         return phoneNumber;
     }
 
-    private static String cleanPhoneNumber(String phoneNumber) {
+    public static String cleanPhoneNumber(String phoneNumber) {
         return phoneNumber.replaceAll("[^0-9]", "");
     }
 
-    private static boolean checkPhoneNumberIsCorrect(String phoneNumber) {
+    public static boolean checkPhoneNumber(String phoneNumber) {
         return cleanPhoneNumber(phoneNumber).length() == 11
                 && (phoneNumber.charAt(0) == '7' || phoneNumber.charAt(0) == '8');
     }
 
-    private static String formatNumber(String phoneNumber) {
+    public static String formatPhoneNumber(String phoneNumber) {
         return "+7" + " " + phoneNumber.substring(1, 4) + " " +
                 phoneNumber.substring(4, 7) + " " + phoneNumber.substring(7, 9) + " " + phoneNumber.substring(9);
     }
 
     public static String[] getName() {
-        String resSting = "";
         String[] nameArray = new String[3];
 //        Scanner inData = new Scanner(System.in);
         Scanner inData = new Scanner("перров петр инванович");
@@ -62,15 +57,14 @@ public class PhoneBook {
             isCorrectName = nameArray.length == 3;
             if (!isCorrectName) {
                 System.out.println("Введите корректное имя!");
-                inData.close();
             }
         }
+        inData.close();
         return nameArray;
     }
 
 
     public static String[] firstCharUp(String[] resStr) {
-        String result = "";
         for (int i = 0; i < resStr.length; i++) {
             char firstChar = resStr[i].charAt(0);
             resStr[i] = Character.toUpperCase(firstChar) + resStr[i].substring(1);
@@ -82,10 +76,6 @@ public class PhoneBook {
         return name.trim().split(" ");
     }
 
-    public static boolean checkPhoneNumber(String phoneNumber) {
-        return true;
-    }
-
     public static boolean checkName(String name) {
         return (splitName(name).length == 3);
     }
@@ -94,9 +84,6 @@ public class PhoneBook {
         return "";
     }
 
-    public static String formatPhoneNumber(String number) {
-        return "";
-    }
 
     public static void add(String[][] book, String name, String number) {
         //add logic
@@ -104,5 +91,14 @@ public class PhoneBook {
 
     public static void list(String[][] book) {
         //print phone book
+    }
+
+    private static String stringArrayToString(String [] array) {
+        String resStr = "";
+        for (int i = 0; i < array.length; i++) {
+            resStr += (array[i] + " ");
+        }
+        resStr = resStr.trim();
+        return resStr;
     }
 }
